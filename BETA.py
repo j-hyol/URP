67e0dd81f548a97e296551e413394a097252b11a
j-hyol <104234985+j-hyol@users.noreply.github.com>
2022-07-07 22:33:29 +0900

    Create BETA.py

---
+++ BETA.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+import pandas as pd
+import talib
+
+df = pd.read_excel('(N=30 _ Freq=1D) Historical Data-GSON.xlsx')
+
+symbol = 'F'
+high = data[f'{symbol}']['High'].values
+low = data[f'{symbol}']['Low'].values
+output = talib.BETA(high, low)
+import matplotlib.pyplot as plt
+
+plt.plot(high)
+plt.plot(low)
+plt.plot(output)
