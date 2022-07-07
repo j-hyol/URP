e87ba47c2ce0f2ea7fb5de54c7aa6857938dab78
j-hyol <104234985+j-hyol@users.noreply.github.com>
2022-07-07 16:37:45 +0900

    Update SAR.py

--- SAR.py
+++ SAR.py
@@ -6,7 +6,7 @@ df = pd.read_excel('(N=30 _ Freq=1D) Historical Data-GSON.xlsx')
 symbol = 'F'
 high = data[f'{symbol}']['High'].values
 low = data[f'{symbol}']['Low'].values
-output = talib.SAR(high, low)
+output = talib.SAR(high, low, acceleration=0, maximum=0)
 
 plt.plot(high)
 plt.plot(low)
